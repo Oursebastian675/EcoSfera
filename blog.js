172b03c17d48e08255da504ff0768b9aab0c3baa
Sebaxtian18 <zsebax.1994@gmail.com>
2024-11-22 08:51:12 -0500

    Se agrega la estructura de los articulos del blog y queda en proceso para generarlo atraves de JS

---
+++ blog.js
@@ -0,0 +1,14 @@
+
+
+function crearCardArticulo(idArticulo) {
+    const articuloSeccion = document.getElementById(idArticulo);
+    let contenedorCard = document.createElement('div');
+    let blogCard = document.createElement('div');
+    let imagenEncabezado = document.createElement('img');
+    let contenedorInformacion = document.createElement('div');
+    let titulo = document.createElement('h2');
+    let contenido = document.createElement('p');
+    let subtituloParrafo1 = document.createElement('h3');
+    let subtituloParrafo2 = document.createElement('h4');
+    let 
+}
